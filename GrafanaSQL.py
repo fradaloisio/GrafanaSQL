3af6e4a93f12237895a33684d33325c8138f54fa
Francesco d'Aloisio <Francesco.DAloisio@serco.com>
2016-05-24 16:33:12 +0200

    Added check if date is String or datetime obj, added columns name in result

--- GrafanaSQL.py
+++ GrafanaSQL.py
@@ -4,14 +4,22 @@
 # /query should return metrics based on input.
 # /annotations should return annotations.
 # '''
-import json, re, time, copy
+import json, re, time, datetime
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from dateutil.parser import parse
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./database_test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+pg8000://user@127.0.0.1:5432/mySpecialDB'
 db = SQLAlchemy(app)
+conn = db.engine.connect()
+
+if conn != None:
+    print "Connected"
+else:
+    print "NOT Connected"
+    exit()
+
 
 #/ should return 200 ok. Used for "Test connection" on the datasource config page.
 @app.route('/')
@@ -35,12 +43,11 @@ def query():
 
     # query?db=test&epoch=ms&q=SELECT mean("value") FROM "it" WHERE time > 1456786800s and time < 1456790400s GROUP BY time(360s) fill(null)
     print("db: %s" %(request.args.get('db')))
+    db = request.args.get('db')
     epoch = request.args.get('epoch')
     print("epoch: %s" %(request.args.get('epoch')))
     print("query: %s" %(request.args.get('q')))
     sql = request.args.get('q')
-
-    conn = db.engine.connect()
 
     for query in sql.split("\n"):
         print query
@@ -48,8 +55,8 @@ def query():
         serie = {}
         values = []
 
-        serie["name"] = "test"
-        serie["columns"]= ["time","mean"]
+        serie["name"] = db # TODO: assign the right value
+        serie["columns"]= ["time","value"]
         if sql != "SHOW MEASUREMENTS":
             timerange = re.search('timerange\(time.?..?(.*?)s and time.?..?(.*?)s.?,.?\'(.*?)\'\)', query).group(1,2,3)
             if timerange:
@@ -59,10 +66,22 @@ def query():
                 query = query.replace("%ss" % timerange[0], "'%s'" % t1)
                 query = re.sub("%ss.?,.?\'%s\'\)" % (timerange[1], timerange[2]), "'%s'" % t2, query)
 
-                rs = conn.execute(query).fetchall()
+                q = conn.execute(query)
+                rs = q.fetchall()
+
+                serie["columns"] = q.keys()
+
                 for r in rs:
-                    t = int(parse(r[0]).strftime('%s')) * 1000 if epoch == "ms" else int(parse(r[0]).strftime('%s'))
-                    values.append([t, r[1]])
+                    result_array = []
+                    if isinstance(r[0], datetime.datetime):
+                        t = int(r[0].strftime('%s')) * 1000 if epoch == "ms" else int(r[0].strftime('%s'))
+                    else:
+                        t = int(parse(r[0]).strftime('%s')) * 1000 if epoch == "ms" else int(parse(r[0]).strftime('%s'))
+                    result_array.append(t)
+
+                    for v in range(1,r.__len__()):
+                        result_array.append(r[v])
+                    values.append(result_array)
 
         elif sql == "SHOW MEASUREMENTS":
             serie["columns"] = ["name"]
@@ -78,7 +97,7 @@ def query():
         results.append({"series": series})
 
     response = {"results":results}
-    print json.dumps(response)
+    #print json.dumps(response)
     return json.dumps(response)
 
 if __name__ == "__main__":
